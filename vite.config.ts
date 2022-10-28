4fa24c833696508c3838859bdda158f878bb855e
Ekaradon <ekaradon+git@protonmail.com>
2022-10-28 10:31:30 +0200

    setup eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [eslint(), react(), dts({ insertTypesEntry: true })],
   publicDir: 'public',
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src', 'lib') }],
